2de739c23e500e8c04e477a8b16e021860d59dbd
Colin <colingoodale@gmail.com>
2019-11-22 10:36:30 -0600

    added in css and template wireframe components

--- client/src/App.js
+++ client/src/App.js
@@ -26,31 +26,35 @@ class Dropdown extends Component {
 
   render() {
     return (
-      <div className="dropdown" style={{ background: "red", width: "200px" }} >
-        <div className="button" onClick={this.showDropdownMenu}> {this.state.currentOperation} </div>
+      <div className="App">
+        <h1>Newtonify</h1>
+        <h3>Choose your operation from the avilable list</h3>
+        <h3>Type in the equation you would like to perform</h3>
+        <div className="dropdown" style={{ background: "red", width: "200px" }} >
+          <div className="button" onClick={this.showDropdownMenu}> {this.state.currentOperation} </div>
 
-        {this.state.displayMenu ? (
+          {this.state.displayMenu ? (
 
-          <ul>
-            {this.state.operations.map(operation => (
-              <OperationListItem
-                id={operation.id}
-                key={operation.id}
-                value={operation.value}
-                hideDropdownMenu={() => this.hideDropdownMenu(operation.value)}
-                text={operation.text}
-              />
-            ))}
-          </ul>
+            <ul>
+              {this.state.operations.map(operation => (
+                <OperationListItem
+                  id={operation.id}
+                  key={operation.id}
+                  value={operation.value}
+                  hideDropdownMenu={() => this.hideDropdownMenu(operation.value)}
+                  text={operation.text}
+                />
+              ))}
+            </ul>
 
-        ) :
-          (
-            null
-          )
-        }
+          ) :
+            (
+              null
+            )
+          }
 
+        </div>
       </div>
-
     );
   }
 }
